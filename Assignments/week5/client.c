fda98ed641a9a5cbfcdfdc53925237db344e3181
Seung Bin . Ko <kosb15@naver.com>
2017-10-06 16:25:58 +0900

    Perform the assignment

--- Assignments/week5/client.c
+++ Assignments/week5/client.c
@@ -15,9 +15,10 @@ void error_handling(char * message);
 
 int main(int argc , char * argv[]){
 
+    /* 선언부 */
     int sock;
     char message[BUF_SIZE];
-    int str_len , recv_len , recv_cnt;
+    ssize_t str_len , recv_len , recv_cnt;
     struct sockaddr_in serv_adr;
 
     if(argc != 3){
@@ -25,15 +26,25 @@ int main(int argc , char * argv[]){
         exit(1);
     }
 
+    /* TCP 소켓 생성 */
     sock = socket(PF_INET , SOCK_STREAM , 0);
     if(sock == -1)  error_handling("socket() error");
 
+    /*
+     * socket address에 주소 정보 초기화 ( 주소체계 - v4 , IP , PORT 초기화 )
+     * 네트워크 전송시, 빅엔디안 방식으로 스트림을 전송하므로 네트워크 바이트 순서로 변환함
+     * */
     memset(&serv_adr , 0 , sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_adr.sin_port = htons(atoi(argv[2]));
 
 
+    /*
+     * server 에 연결요청을 보냄
+     * server 의 연결요청 대기 큐에 삽입이 되면 성공
+     * socket 에 서버의 주소정보를 자동으로 등록함
+     * */
     if(connect(sock , (struct sockaddr*) &serv_adr , sizeof(serv_adr)) == -1)
     {
         error_handling("connet() error");
@@ -49,26 +60,30 @@ int main(int argc , char * argv[]){
 
         if(!strcmp(message , "q\n") || !strcmp(message , "Q\n"))    break;
 
+        /*
+         * server 에 데이터 전송
+         * */
         str_len = write(sock , message , strlen(message));
 
         recv_len = 0;
 
         /*
-         * 데이터의 경계가 없기 때문에, 보낸 데이터 크기만큼 반복하여 버퍼에 채움
+         * 데이터의 경계가 없기 때문에, 보낸 데이터 크기만큼 될 때까지 반복하여 데이터를 읽음
          *
          * */
         while(recv_len < str_len)
         {
             recv_cnt = read(sock , &message[recv_len] , BUF_SIZE - 1);
-            if(recv_cnt == -1)
-                error_handling("read() error!");
+            if(recv_cnt == -1)  error_handling("read() error!");
             recv_len += recv_cnt;
         }
 
+        /* null pointer */
         message[recv_len] = 0;
         printf("Message from server : %s" , message);
     }
 
+    /* 소켓 종료 */
     close(sock);
     return 0;
 }
--- Assignments/week5/server.c
+++ Assignments/week5/server.c
@@ -14,38 +14,53 @@ void error_handling(char * message);
 
 int main(int argc , char* argv[])
 {
-
+    /* 선언부 */
     int serv_sock , clnt_sock;
-
+    int i;
     char message[BUF_SIZE];
-    int str_len , i;
-
     struct sockaddr_in serv_adr , clnt_adr;
     socklen_t  clnt_adr_sz;
+    ssize_t str_len;
 
     if(argc != 2){
         printf("Usage : %s <port>\n", argv[0]);
         exit(1);
     }
 
-    serv_sock = socket(PF_INET , SOCK_STREAM , 0);
-    if(serv_sock == -1)
+    /*
+     * TCP Socket 생성
+     *
+     * */
+    if((serv_sock = socket(PF_INET , SOCK_STREAM , 0)) == -1)
     {
         error_handling("socket() error");
     }
 
+    /*
+     * socket address에 주소 정보 초기화 ( 주소체계 - v4 , IP , PORT 초기화 )
+     * 네트워크 전송시, 빅엔디안 방식으로 스트림을 전송하므로 네트워크 바이트 순서로 변환함
+     * INADDR_ANY : 서버 IP를 자동으로 할당하는 변수
+     * 서버 주소를 담는 구조체를 0으로 초기화
+     * */
     memset(&serv_adr , 0 , sizeof(serv_adr));
     serv_adr.sin_family = AF_INET;
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_adr.sin_port = htons(atoi(argv[1]));
 
-
-
+    /*
+     * 초기화된 주소 정보를 socket에 할당
+     *
+     * */
     if(bind(serv_sock , (struct sockaddr*) &serv_adr , sizeof(serv_adr)) == -1)
     {
         error_handling("bind() error");
     }
 
+    /*
+     * 크기가 5인 연결 요청 대기 큐 생성
+     * 연결요청 대기 큐에 삽입되면, 서버 소켓이 됨
+     * 연결 요청 대기 큐에 삽입되면, client는 connect 성공으로 받아들임
+     * */
     if(listen(serv_sock , 5) == -1)
     {
         error_handling("listen() error");
@@ -55,26 +70,49 @@ int main(int argc , char* argv[])
 
     for(i = 0 ; i < 5; i++)
     {
+        /*
+         * 연결요청 수락
+         * 연결요청 대기 큐에서 대기 중인 client socket descriptor 하나를 꺼내와 연결요청을 수락
+         * client socket 하나를 생성하고 정보를 자동으로 주입함
+         * client 와 연결됨
+         * */
         clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_adr , &clnt_adr_sz);
+
         if(clnt_sock == -1)
         {
             error_handling("accept() error");
         }
         else{
+
+            /*
+             * 주입받은 client 정보 출력
+             * 네트워크 바이트 순서를 호스트 바이트 순서로 바꿔서 출력
+             * */
+            printf("Connected client : %s %d \n" , inet_ntoa(clnt_adr.sin_addr) , ntohs(clnt_adr.sin_port));
             printf("Connected client %d \n" , i + 1);
-            printf("Connected IP : %s \n" , inet_ntoa(clnt_adr.sin_addr));
-            printf("Connected PORT : %d \n" , ntohs(clnt_adr.sin_port));
         }
 
 
+        /*
+         * 연결된 client로 부터 데이터를 입력받음
+         * client로 데이터 전송
+         * TCP는 데이터의 경계가 없어, 입력을 여러번에 나눠서 받을 수 있음
+         * */
         while((str_len = read(clnt_sock , message , BUF_SIZE)) != 0)
         {
             write(clnt_sock , message , str_len);
         }
 
+        /*
+         * client 소켓 종료
+         * client에게 연결 종료를 전달
+         * */
         close(clnt_sock);
     }
 
+    /*
+     * server 소켓 종료
+     * */
     close(serv_sock);
     return 0;
 }
